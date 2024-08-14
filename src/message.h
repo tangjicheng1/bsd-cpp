adae6386a4dfe6888bd404ba20b4c0d745c68db2
tang <tangjch15@gmail.com>
2024-08-14 15:10:20 +0900

    modified:   src/message.h

--- src/message.h
+++ src/message.h
@@ -1,12 +1,21 @@
-#include <cstdint>
-#include <cstring>
+// message.h
+#ifndef MESSAGE_H
+#define MESSAGE_H
 
-constexpr size_t MAX_MESSAGE_BODY_LENGTH = 1024;
+#include <cstdint>
+
+const int MAX_BODY_LENGTH = 1024; // 定义消息体的最大长度
 
 #pragma pack(push, 1)
 struct Message {
     uint16_t length;
     char message_type;
-    char body[MAX_MESSAGE_BODY_LENGTH];
+    char body[MAX_BODY_LENGTH]; // 使用常量定义消息体长度
 };
 #pragma pack(pop)
+
+// 定义一些消息类型常量
+const char MESSAGE_TYPE_HELLO = 'H';
+const char MESSAGE_TYPE_HEARTBEAT = 'B';
+
+#endif // MESSAGE_H
