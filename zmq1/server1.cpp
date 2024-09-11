c7ef1437104ff641372fd8dda27e6a75479946fd
tangjicheng <tangjch15@gmail.com>
2024-09-11 11:09:52 +0900

    modified:   zmq1/client1.cpp 	modified:   zmq1/server1.cpp

--- zmq1/server1.cpp
+++ zmq1/server1.cpp
@@ -11,17 +11,14 @@ int main() {
     socket.bind("tcp://*:5555");
 
     while (true) {
-        // 接收客户端的请求
         zmq::message_t request;
         socket.recv(request, zmq::recv_flags::none);
         std::string req_str(static_cast<char*>(request.data()), request.size());
-        std::cout << "接收到的请求: " << req_str << std::endl;
+        std::cout << "server recv: " << req_str << std::endl;
 
-        // 模拟处理请求
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        // 发送响应给客户端
-        std::string reply = "世界，你好";
+        std::string reply = "client, hello";
         zmq::message_t response(reply.size());
         memcpy(response.data(), reply.c_str(), reply.size());
         socket.send(response, zmq::send_flags::none);
