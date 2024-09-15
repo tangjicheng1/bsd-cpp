05811d3f368318e31dc9c0108c4f7990495ab721
tangjicheng <tangjch15@gmail.com>
2024-09-15 16:38:44 +0900

    modified:   nanobind/run.sh

--- nanobind/run.sh
+++ nanobind/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+## prepare
+pip install nanobind
+python -m nanobind --cmake_dir
+python -m nanobind --include
+
+## build
+mkdir build
+cd build
+cmake ..
+make
+
+## run
 cp ./my.py ./build
 python my.py
