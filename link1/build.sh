6869bd4e4852ff2508eb1ae66b83bac97c7cc8fc
tangjicheng <tangjch15@gmail.com>
2024-09-29 16:43:53 +0900

    modified:   link1/build.sh 	new file:   link1/my_export.c

--- link1/build.sh
+++ link1/build.sh
@@ -5,3 +5,5 @@ gcc -c print2.c -o print2.o
 ar rcs liball2.a print2.o print1.o
 
 gcc main.c liball.a -o main_exe
+
+gcc -c my_export.c -o my_export.o
---
+++ link1/my_export.c
@@ -0,0 +1,7 @@
+// __attribute__((visibility("hidden"))) void print1();
+// __attribute__((visibility("default"))) void print2();
+
+static void print1() {
+}
+void print2() {
+}
