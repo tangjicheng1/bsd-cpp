53a4bcd93dc20feb915c42af2cf2fdc7728ed811
tangjicheng <tangjch15@gmail.com>
2024-10-04 16:14:11 +0900

    modified:   lua1/build.sh

--- lua1/build.sh
+++ lua1/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 luastatic ./fibonacci.lua /usr/local/lib/libluajit-5.1.a -I/usr/local/include -o fib_jit
+
+sudo luarocks install luasocket
+sudo luarocks install luastatic
+sudo luarocks install lua-cjson
+
+sudo luarocks install luasql-sqlite3
+sudo luarocks install luasql-postgres
+sudo luarocks install luasql-mysql
+sudo luarocks install luasql-sqlite
+sudo luarocks install luasql-odbc
