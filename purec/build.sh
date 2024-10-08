597bd352936234f1d106b9db506242ef3f4bfd45
tangjicheng <tangjch15@gmail.com>
2024-10-08 23:35:52 +0900

    add c

---
+++ purec/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -s url | gcc -std=c99 -o mybatch -x c -
---
+++ purec/mybatch.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_CMD_LENGTH 2048
+#define MAX_ARGS 256
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s command.txt\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Can NOT open command file");
+        return EXIT_FAILURE;
+    }
+
+    char command[MAX_CMD_LENGTH];
+    pid_t pids[MAX_ARGS];
+    int count = 0;
+
+    while (fgets(command, sizeof(command), file) != NULL) {
+        command[strcspn(command, "\n")] = 0;
+
+        if (strlen(command) == 0) {
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            // failed
+            perror("Can NOT fork process");
+            fclose(file);
+            return EXIT_FAILURE;
+        } else if (pid == 0) {
+            // in child process
+            char* args[MAX_ARGS];
+            char* token = strtok(command, " ");
+            int i = 0;
+
+            while (token != NULL && i < MAX_ARGS - 1) {
+                args[i++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[i] = NULL;
+
+            execvp(args[0], args);
+            perror("Execute command failed");
+            printf("Failed command: %s\n", command);
+            exit(EXIT_FAILURE);
+        } else {
+            // in parent process
+            pids[count++] = pid;
+        }
+    }
+
+    fclose(file);
+
+    for (int i = 0; i < count; i++) {
+        waitpid(pids[i], NULL, 0);
+    }
+
+    return EXIT_SUCCESS;
+}
