db661a89572e2d8a19ba59a7559a5fa7e7e27c5a
tangjicheng <tangjch15@gmail.com>
2024-10-08 23:37:31 +0900

    add build.sh

--- purec/build.sh
+++ purec/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s url | gcc -std=c99 -o mybatch -x c -
+curl -s https://raw.githubusercontent.com/tangjicheng1/demo-cpp/refs/heads/main/purec/mybatch.c | gcc -std=c99 -o mybatch -x c -
